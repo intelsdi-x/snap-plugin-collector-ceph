399f3f27423068878a519a14901c771756ad07b6
Mateusz Kleina <mateusz.kleina@intel.com>
2016-05-06 12:36:54 +0200

    Add support for dynamic metrics

--- ceph/ceph.go
+++ ceph/ceph.go
@@ -28,8 +28,8 @@ import (
 	"os/exec"
 	"path/filepath"
 	"reflect"
+	"regexp"
 	"strings"
-	"time"
 
 	"github.com/intelsdi-x/snap/control/plugin"
 	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
@@ -48,20 +48,24 @@ const (
 
 	// Daemon name index in metrics namespace: [intel storage ceph daemonName]
 	daemonNameIndex = 3
+	daemonIDIndex   = 4
 
 	// Default path to ceph executable
-	cephBinPathDefault = "/usr/bin/ceph"
+	//cephBinPathDefault = "/usr/bin/ceph"
+	cephBinPathDefault = "/home/mkleina/ceph/src"
 
 	// Default Ceph's socket config
-	socketPathDefault   = "/var/run/ceph"
-	socketPrefixDefault = "ceph-"
+	//socketPathDefault   = "/var/run/ceph"
+	socketPathDefault = "/home/mkleina/ceph/src/out"
+	//socketPrefixDefault = "ceph-"
+	socketPrefixDefault = ""
 	socketExtDefault    = "asok"
 )
 
 // Ceph
 type Ceph struct {
 	path        string // path to ceph executable
-	keys        []string
+	keys        []core.Namespace
 	daemons     []string
 	socket      Socket
 	initialized bool // after init() plugin with Config set true to avoid reinitalization
@@ -84,78 +88,155 @@ type RealCmd struct{}
 
 var cmd Command
 
-// execute command
-func (c *RealCmd) perfDump(command string, args ...string) ([]byte, error) {
-	return exec.Command(command, args...).Output()
+// New() returns Snap-Plugin-Collector-Ceph instance
+func New() *Ceph {
+	ceph := &Ceph{initialized: false}
+	cmd = &RealCmd{}
+
+	return ceph
 }
 
-func (c *RealCmd) lookPath(file string) (string, error) {
-	return exec.LookPath(file)
+// Init() initalizes ceph plugin, gets information about running ceph-daemons and available metrics
+func (ceph *Ceph) Init(config map[string]ctypes.ConfigValue) error {
+	if ceph.initialized {
+		return nil
+	}
+
+	// set ceph conf param
+	ceph.path = getCephBinaryPath(config)
+	ceph.socket = getCephSocketConf(config)
+
+	// get ceph daemon names based on socket details
+	ceph.daemons = ceph.socket.getCephDaemonNames()
+	if len(ceph.daemons) <= 0 {
+		return fmt.Errorf("Can not get Ceph Daemon Name(s) - check if any Ceph Daemon is running")
+	}
+
+	dkeys := make(map[string][]string)
+
+	for _, daemon := range ceph.daemons {
+		socket := filepath.Join(ceph.socket.path, daemon)
+
+		// perf dump command is `/path/to/ceph/bin --admin-daemon /path/to/exemplary/socket/osd.0.asok perf dump`
+		out, err := cmd.perfDump(filepath.Join(ceph.path, "ceph"), "--admin-daemon", socket, "perf", "dump")
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error execution of ceph-daemon perf dump command for socket %+v, err=%+v\n",
+				socket, err)
+			return err
+		}
+
+		if keys, err := parsePerfDumpOut(out); err == nil {
+			dkeys[daemon] = keys
+		} else {
+			fmt.Fprintf(os.Stderr, "Error parsing output of ceph-daemon perf dump command for socket %+v, err=%+v\n",
+				socket, err)
+		}
+
+	}
+
+	if len(dkeys) == 0 {
+		return fmt.Errorf("No Ceph metrics available")
+	}
+
+	ceph.keys = []core.Namespace{}
+
+	for _, daemon := range ceph.daemons {
+		for _, key := range dkeys[daemon] {
+			daemonName := trimPrefixAndSuffix(daemon, ceph.socket.prefix, "."+ceph.socket.ext)
+			cephNs := daemonName + key
+			ceph.keys = append(ceph.keys, createNamespace(cephNs))
+		}
+	}
+
+	ceph.initialized = true
+	return nil
 }
 
-// trimPrefixAndSuffix returns 's' without the provided prefix and suffix strings.
-// If 's' neither starts with prefix, nor ends with suffix, 's' is returned unchanged.
-func trimPrefixAndSuffix(s string, prefix string, suffix string) string {
-	s = strings.TrimPrefix(s, prefix)
-	s = strings.TrimSuffix(s, suffix)
-	return s
-}
-
-// getCephDaemonMetrics executes "ceph --admin-daemon perf dump" command for defined daemon-socket and returns its metrics
-func (c *Ceph) getCephDaemonMetrics(mts []plugin.MetricType, daemon string) ([]plugin.MetricType, error) {
-	out, err := cmd.perfDump(filepath.Join(c.path, "ceph"), "--admin-daemon", filepath.Join(c.socket.path, daemon),
+// GetCephDaemonMetrics executes "ceph --admin-daemon perf dump" command for defined daemon-socket and returns its metrics
+func (ceph *Ceph) GetCephDaemonMetrics(mts []plugin.MetricType, daemon string) ([]plugin.MetricType, error) {
+	out, err := cmd.perfDump(filepath.Join(ceph.path, "ceph"), "--admin-daemon", filepath.Join(c.socket.path, daemon),
 		"perf", "dump")
-	timestamp := time.Now()
+	//timestamp := time.Now()
+	//hostname, _ := os.Hostname()
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Ceph perf dump command execution failed for socket %+v, err=%+v\n",
-			filepath.Join(c.socket.path, daemon), err)
+			filepath.Join(ceph.socket.path, daemon), err)
 		return nil, err
 	}
 
-	var dat map[string]interface{}
+	var jsonData map[string]interface{}
 
-	if err := json.Unmarshal(out, &dat); err != nil {
+	if err := json.Unmarshal(out, &jsonData); err != nil {
 		fmt.Fprintf(os.Stderr, "Cannot unmarshal JSON object from ceph-daemon socket, err=%+v\n", err)
 		return nil, err
 	}
 
 	metrics := []plugin.MetricType{}
+	jsonMetrics := make(map[string]interface{})
 
 	for _, m := range mts {
-		nlen := len(m.Namespace())
-		daemonName := trimPrefixAndSuffix(daemon, c.socket.prefix, "."+c.socket.ext)
+		daemonName := trimPrefixAndSuffix(daemon, ceph.socket.prefix, "."+ceph.socket.ext)
 
-		// compare daemonName with proper component of metric's namespace [intel storage ceph daemonName]
-		if daemonName == m.Namespace()[daemonNameIndex].Value {
-			// get metrics defined in task for this daemon
-			dat_r := dat
-			// skip the const components of metrics namespace
-			for _, name := range m.Namespace()[daemonNameIndex+1 : nlen-1] {
-				if dat_r[name.Value] == nil {
-					break
-				}
-				dat_r = dat_r[name.Value].(map[string]interface{}) // get metric
-			}
+		// Match daemon name with namespace, for example osd/* is formatted as osd.* wchich matches osd.1, osd.2, osd.3 etc
+		matchDaemon, _ := regexp.MatchString(strings.Join(m.Namespace().Strings()[daemonNameIndex:daemonIDIndex], `\.`), daemonName)
 
-			hostname, _ := os.Hostname()
-			metric := plugin.MetricType{
-				Namespace_: m.Namespace(),
-				Data_:      dat_r[m.Namespace()[nlen-1].Value], // get value of metric
-				Tags_:      map[string]string{"source": hostname + "/" + daemonName},
-				Timestamp_: timestamp,
-			}
+		// Get metrics defined in task for this daemon
+		if matchDaemon {
+			ceph.getJsonDataByNamespace(jsonData, m.Namespace().Strings()[daemonIDIndex+1:], []string{}, &jsonMetrics)
 
-			metrics = append(metrics, metric)
+			fmt.Printf("%+v", jsonMetrics)
+
+			// metric := plugin.MetricType{
+			// 	Namespace_: m.Namespace(),
+			// 	Data_:      value, // get value of metric
+			// 	Tags_:      map[string]string{core.STD_TAG_PLUGIN_RUNNING_ON: hostname + "/" + daemonName},
+			// 	Timestamp_: timestamp,
+			// }
+
+			// metrics = append(metrics, metric)
 		}
 	}
 
-	if len(metrics) <= 0 {
-		// do not find desired metrics
-		return nil, errors.New("Do not find desired metrics for ceph-deamon")
+	return metrics, nil
+}
+
+func sliceMatch(a, b []string) bool {
+	match := true
+
+	for i := range a {
+		if a[i] != b[i] && a[i] != "*" && b[i] != "*" {
+			match = false
+		}
 	}
 
-	return metrics, nil
+	return match
+}
+
+func (ceph *Ceph) getJsonDataByNamespace(data map[string]interface{}, namespace []string, resultNamespace []string, results *map[string]interface{}) {
+	fmt.Println("\n---------------------- NEW RECURRENCY CALL")
+	fmt.Println("Namespace: ", namespace)
+	// Go through all JSON data keys
+	for key, _ := range data {
+		fmt.Println("> Checking key ", key, " with namespace ", namespace)
+
+		// Convert ceph key to namespace slice for comparsion
+		keyNs := strings.Split(key, ".")
+
+		if sliceMatch(namespace[0:len(keyNs)], keyNs) {
+			fmt.Println("\t> MATCH", keyNs)
+
+			if reflect.ValueOf(data[key]).Kind() == reflect.Map {
+				// Go deeper into JSON structure
+				ceph.getJsonDataByNamespace(data[key].(map[string]interface{}), namespace[len(keyNs):], append(resultNamespace, keyNs...), results)
+				fmt.Println("BACK IN RECURRENCY PARENT")
+			} else {
+				fmt.Println("============================================ GOT IT!")
+				(*results)[strings.Join(resultNamespace, "/")] = data[key]
+			}
+		}
+	}
 }
 
 // CollectMetrics returns all desired Ceph metrics defined in task manifest
@@ -167,15 +248,12 @@ func (ceph *Ceph) CollectMetrics(mts []plugin.MetricType) ([]plugin.MetricType, 
 	metrics := []plugin.MetricType{}
 
 	// init ceph plugin with Config settings (only once)
-	if ceph.initialized == false {
-		if err := ceph.init(mts[0].Config().Table()); err != nil {
-			return nil, err
-		}
-		ceph.initialized = true
+	if err := ceph.Init(mts[0].Config().Table()); err != nil {
+		return nil, err
 	}
 
 	for _, daemon := range ceph.daemons {
-		if dmetric, err := ceph.getCephDaemonMetrics(mts, daemon); err == nil {
+		if dmetric, err := ceph.GetCephDaemonMetrics(mts, daemon); err == nil {
 			metrics = append(metrics, dmetric...)
 		}
 	}
@@ -186,20 +264,20 @@ func (ceph *Ceph) CollectMetrics(mts []plugin.MetricType) ([]plugin.MetricType, 
 // GetMetricTypes returns the metric types exposed by ceph-daemon sockets
 func (ceph *Ceph) GetMetricTypes(cfg plugin.ConfigType) ([]plugin.MetricType, error) {
 	// init ceph plugin with Global Config params
-	if err := ceph.init(cfg.Table()); err != nil {
+	if err := ceph.Init(cfg.Table()); err != nil {
 		return nil, err
 	}
 
-	mts := make([]plugin.MetricType, len(ceph.keys))
-	for i, k := range ceph.keys {
-		mts[i] = plugin.MetricType{Namespace_: core.NewNamespace(strings.Split(strings.TrimPrefix(k, "/"), "/")...)}
+	mts := []plugin.MetricType{}
+	for _, k := range ceph.keys {
+		mts = append(mts, plugin.MetricType{Namespace_: k})
 	}
 
 	return mts, nil
 }
 
 // GetConfigPolicy returns a ConfigPolicy
-func (c *Ceph) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
+func (ceph *Ceph) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
 	p := cpolicy.New()
 
 	return p, nil
@@ -253,6 +331,24 @@ func getCephSocketConf(config map[string]ctypes.ConfigValue) Socket {
 	return s
 }
 
+// createNamespace returns namespace slice of strings composed from: vendor, class, type, ceph daemon name and ceph daemon id
+func createNamespace(cephNs string) core.Namespace {
+	result := core.NewNamespace([]string{ns_vendor, ns_class, ns_type}...)
+	cephNsSplit := strings.Split(cephNs, "/")
+
+	for _, cephNsEntry := range cephNsSplit {
+		// Split metric name into static element before dot and dynamic elements after
+		idSplit := strings.Split(cephNsEntry, ".")
+		staticElement := idSplit[0]
+
+		result = result.AddStaticElement(staticElement)
+		for _, dynamicElement := range idSplit[1:] {
+			result = result.AddDynamicElement(dynamicElement, "ID of "+staticElement)
+		}
+	}
+	return result
+}
+
 // getCephDaemonNames scans the path to ceph sockets in search of an instance which name contains specified prefix and extension.
 // Returns names of available ceph-daemon sockets
 func (s *Socket) getCephDaemonNames() []string {
@@ -271,6 +367,10 @@ func (s *Socket) getCephDaemonNames() []string {
 	}
 
 	return names
+}
+
+func (c *RealCmd) lookPath(file string) (string, error) {
+	return exec.LookPath(file)
 }
 
 // parseMapToNamespace returns a slice contains metrics's namespace. As input there is map of strings to arbitrary data and current
@@ -309,65 +409,15 @@ func parsePerfDumpOut(cmdOut []byte) ([]string, error) {
 	return keys, nil
 }
 
-// Init() initalizes ceph plugin, gets information about running ceph-daemons and available metrics
-func (ceph *Ceph) init(config map[string]ctypes.ConfigValue) error {
-	// set ceph conf param
-	ceph.path = getCephBinaryPath(config)
-	ceph.socket = getCephSocketConf(config)
-
-	// get ceph daemon names based on socket details
-	ceph.daemons = ceph.socket.getCephDaemonNames()
-	if len(ceph.daemons) <= 0 {
-		return fmt.Errorf("Can not get Ceph Daemon Name(s) - check if any Ceph Daemon is running")
-	}
-
-	dkeys := make(map[string][]string)
-
-	for _, daemon := range ceph.daemons {
-		socket := filepath.Join(ceph.socket.path, daemon)
-
-		// perf dump command is `/path/to/ceph/bin --admin-daemon /path/to/exemplary/socket/osd.0.asok perf dump`
-		out, err := cmd.perfDump(filepath.Join(ceph.path, "ceph"), "--admin-daemon", socket, "perf", "dump")
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error execution of ceph-daemon perf dump command for socket %+v, err=%+v\n",
-				socket, err)
-			return err
-		}
-
-		if keys, err := parsePerfDumpOut(out); err == nil {
-			dkeys[daemon] = keys
-		} else {
-			fmt.Fprintf(os.Stderr, "Error parsing output of ceph-daemon perf dump command for socket %+v, err=%+v\n",
-				socket, err)
-		}
-
-	}
-
-	if len(dkeys) == 0 {
-		return fmt.Errorf("No Ceph metrics available")
-	}
-
-	ceph.keys = []string{}
-
-	for _, daemon := range ceph.daemons {
-		for _, key := range dkeys[daemon] {
-			daemonName := trimPrefixAndSuffix(daemon, ceph.socket.prefix, "."+ceph.socket.ext)
-			ceph.keys = append(ceph.keys, strings.Join(createNamespace(daemonName+key), "/"))
-		}
-	}
-	return nil
+// execute command
+func (c *RealCmd) perfDump(command string, args ...string) ([]byte, error) {
+	return exec.Command(command, args...).Output()
 }
 
-// New() returns Snap-Plugin-Collector-Ceph instance
-func New() *Ceph {
-	ceph := &Ceph{initialized: false}
-	cmd = &RealCmd{}
-
-	return ceph
-}
-
-// createNamespace returns namespace slice of strings composed from: vendor, class, type and ceph-daemon name
-func createNamespace(name string) []string {
-	return []string{ns_vendor, ns_class, ns_type, name}
+// trimPrefixAndSuffix returns 's' without the provided prefix and suffix strings.
+// If 's' neither starts with prefix, nor ends with suffix, 's' is returned unchanged.
+func trimPrefixAndSuffix(s string, prefix string, suffix string) string {
+	s = strings.TrimPrefix(s, prefix)
+	s = strings.TrimSuffix(s, suffix)
+	return s
 }
